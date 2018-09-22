dc8a62b962f96ecd353de43c2c2099c771266194
Nikhil Kokra <nikhil.kokra170@gmail.com>
2018-09-22 18:21:22 -0400

    asdf

--- Main.py
+++ Main.py
@@ -1,6 +1,14 @@
 
+import sched, time
+
+def trade():
+    print("period " + str(time.time()))
 
 def main():
-    print("Hello world")
+    period = 2
+
+    while(True):
+        trade()
+        time.sleep(period)
 
 main()
